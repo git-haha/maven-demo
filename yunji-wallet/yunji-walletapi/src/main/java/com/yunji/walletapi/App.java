a61991391a79c515f75b654d0cd46d5d52f66dc1
haha <1835351083@qq.com>
2018-05-10 17:55:28 +0800

    add project to git

---
+++ yunji-wallet/yunji-walletapi/src/main/java/com/yunji/walletapi/App.java
@@ -0,0 +1,13 @@
+package com.yunji.walletapi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
