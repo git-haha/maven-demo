359c2e098fcaae974227dd7f5e57b714d633db7e
haha <1835351083@qq.com>
2019-03-07 09:57:36 +0800

    20190307 hello world! 15:35

--- yunji-wallet/yunji-walletapi/src/main/java/com/yunji/walletapi/App.java
+++ yunji-wallet/yunji-walletapi/src/main/java/com/yunji/walletapi/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! 20190305" );
         System.out.println( "Hello World! 20190305 15:35" );
+        System.out.println( "Hello World! 20190307 15:35" );
     }
 }
